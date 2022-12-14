d2ac8b76fccf1ab341316d37153c93a1620ea004
SPaganoDoval <dovalsonia@gmail.com>
2022-12-14 18:33:51 +0000

    falta detalles esteticos

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = generador();
 };
+function generador() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoI = Math.floor(Math.random() * who.length);
+  let whatI = Math.floor(Math.random() * what.length);
+  let whenI = Math.floor(Math.random() * when.length);
+
+  return who[whoI] + " " + what[whatI] + " " + when[whenI] + " ";
+}
+
+console.log(generador);
